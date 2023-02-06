eb481de85c98310b86e826aeaab98517856673c5
Gantulga G <limited.tulgaa@gmail.com>
2023-02-06 21:03:24 +0800

    added async page title

--- server/app.py
+++ server/app.py
@@ -1,10 +1,11 @@
+import grequests
 from flask import Flask, request, jsonify
 from google_search import Google
 from web_parser import Webparser
 from model import Model
 import nltk
 from flask_cors import CORS
-
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 CORS(app)
@@ -23,19 +24,45 @@ def returnStatus(data: str, code: int):
     return res
 
 
+def returnPrediction(text: str, length: int = 0):
+    # split text sentence by sentence
+    sentences = nltk.tokenize.sent_tokenize(text)
+    if length:
+        length = min(length, len(sentences))
+    else:
+        length = None
+
+    # Model expects maximum 512 words in one sentence
+    valid_sentences = [sentence for sentence in sentences[:length] if len(nltk.tokenize.word_tokenize(sentence)) <= 512]
+
+    env_claim_preds = model.predict_environmental_claim(valid_sentences)
+    fact_check_preds = model.predict_fact_check(valid_sentences)
+    return jsonify({"env_preds": env_claim_preds,
+                    "fact_preds": fact_check_preds,
+                    "sentences": valid_sentences})
+
+
 @app.route('/')
 def index():
     return 'Server Test'
 
 
 @app.get('/search')
-def search():
+async def search():
     query = request.args.get('query', None)
     count = request.args.get('count', 5, type=int)
     news = request.args.get('news', False, type=bool)
     if query:
         res = google.search(query, count, news=news)
-        return jsonify({"res": res, "news": news})
+        rs = (grequests.get(url) for url in res)
+        titles = []
+        rss = grequests.map(rs)
+        for resp in rss:
+            if resp:
+                titles.append(BeautifulSoup(resp.text, 'html.parser').find('title').get_text())
+            else:
+                titles.append('none')
+        return jsonify({"res": res, "news": news, "count": count, "title": titles})
     return returnStatus("Bad request - no query provided", 400)
 
 
@@ -74,18 +101,7 @@ def predict_from_url():
     # even body not found return error response
     if not text:
         return returnStatus("Bad request - given 'url' not responded", 400)
-    # split text sentence by sentence
-    sentences = nltk.tokenize.sent_tokenize(text)
-    length = min(30, len(sentences))
-
-    # Model expects maximum 512 words in one sentence
-    valid_sentences = [sentence for sentence in sentences[:length] if len(nltk.tokenize.word_tokenize(sentence)) <= 512]
-
-    env_claim_preds = model.predict_environmental_claim(valid_sentences)
-    fact_check_preds = model.predict_fact_check(valid_sentences)
-    return jsonify({"env_preds": env_claim_preds,
-                    "fact_preds": fact_check_preds,
-                    "sentences": valid_sentences})
+    return returnPrediction(text, length=10)
 
 
 @app.post('/predict-text')
@@ -97,14 +113,4 @@ def predict_from_text():
         return returnStatus("Bad request - key 'text' not found", 400)
     if not text:
         return returnStatus("Bad request - 'text' has no value", 400)
-    # split text sentence by sentence
-    sentences = nltk.tokenize.sent_tokenize(text)
-
-    # Model expects maximum 514 words in one sentence
-    valid_sentences = [sentence for sentence in sentences if len(nltk.tokenize.word_tokenize(sentence)) <= 512]
-
-    env_claim_preds = model.predict_environmental_claim(valid_sentences)
-    fact_check_preds = model.predict_fact_check(valid_sentences)
-    return jsonify({"env_preds": env_claim_preds,
-                    "fact_preds": fact_check_preds,
-                    "sentences": valid_sentences})
+    return returnPrediction(text)
